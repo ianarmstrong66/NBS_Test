cdc698b0301c3f42612fd3bf1ca0690f02e0173f
Ian Armstrong <ian.armstong66@ntlworld.com>
2018-08-06 20:20:42 +0100

    Interview Question 2

--- IanAQ2IntAns/Program.cs
+++ IanAQ2IntAns/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IanAQ2IntAns
 {
@@ -10,6 +6,55 @@ namespace IanAQ2IntAns
     {
         static void Main(string[] args)
         {
+            Fix_Solution test = new Fix_Solution();
+            int X = 7, Y = 3, response = -1;
+            //int[] A = { 13, 13, 6, 8, 4, 7, 14, 8, 7, 7, 6 };
+            int[] A = {0, 13, 13, 7, 3, 6, 8, 4, 7, 14, 8, 3, 7, 7, 13, 5, 13, 3, 8, 4, 1, 6 };
+            response = test.Solution(X, Y, A);
+            if (response == -1)
+            {
+                Console.WriteLine("The equal occurances in the array have not been established");
+            }
+            else
+            {
+                Console.WriteLine("The longest leading fragment of array in which there is an equal number of occurrences is {0}", response);
+            }
+            Console.WriteLine("Press Enter to close");
+            Console.ReadLine();
+        }
+    }
+    public class Fix_Solution
+    {
+        public int Solution(int X, int Y, int[] A)
+        {
+            int N = A.Length;
+            int nX = 0;
+            int nY = 0;
+            int result = -1;
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] == X)
+                    nX += 1;
+                else if (A[i] == Y)
+                    nY += 1;
+                if (nX == nY && nX != 0)  // Ensure that no correct matchs are not part of the prefix
+                    result = i;
+            }
+            return result;
+            //int N = A.Length;         //The original with bugs.
+            //int nX = 0;
+            //int nY = 0;
+            //int result = -1;
+            //for (int i = 0; i < N; i++)
+            //{
+            //    if (A[i] == X)
+            //        nX += 1;
+            //    else if (A[i] == Y)
+            //        nY += 1;
+            //    if (nX == nY)
+            //        result = i;
+            //}
+            //return result;
         }
     }
 }
