db045d4f9b12579de9dacf8ad7c05e1df432a6c0
ianar <ian.armstrong66@ntlworld.com>
2019-01-20 16:54:54 +0000

    btree

--- IanAQ2IntAns/Program.cs
+++ IanAQ2IntAns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Collections;
 
 namespace IanAQ2IntAns
 {
@@ -10,12 +11,28 @@ namespace IanAQ2IntAns
         {
             Fix_Solution test = new Fix_Solution();
             Fish_Solution test2 = new Fish_Solution();
+            Tree_Solution test3 = new Tree_Solution();
             Random RndA = new Random();
             Random RndB = new Random();
             int X = 0, Y = 3, Z = 100000000, response = -1, response2 = -1;
             //int[] A = { 13, 13, 6, 8, 4, 7, 14, 8, 7, 7, 6 };
-            int[] A = {0, 13, 13, 7, 3, 6, 8, 4, 7, 14, 8, 3, 7, 7, 0, 13, 5, 13, 3, 8, 4, 1, 6 };
+            int[] A = { 0, 13, 13, 7, 3, 6, 8, 4, 7, 14, 8, 3, 7, 7, 0, 13, 5, 13, 3, 8, 4, 1, 6 };
+
             Stopwatch _stopwatch = new Stopwatch();
+            Tree TreeA = new Tree();
+            TreeA = test3.AddNewNode(5);
+            TreeA.l = test3.AddNewNode(4);
+            TreeA.r = test3.AddNewNode(14);
+            TreeA.l.l = test3.AddNewNode(4);
+            TreeA.l.r = test3.AddNewNode(3);
+            TreeA.l.r.r = test3.AddNewNode(2);
+            TreeA.l.r.l = test3.AddNewNode(12);
+            TreeA.l.r.l.r = test3.AddNewNode(5);
+            TreeA.l.r.r.l = test3.AddNewNode(32);
+            TreeA.l.r.r.r = test3.AddNewNode(42);
+
+
+
             // Run first test
             response = test.Solution(X, Y, A);
             if (response == -1)
@@ -33,8 +50,8 @@ namespace IanAQ2IntAns
             //{
             //    C[i] = RndA.Next(1, 100000);
             //    D[i] = RndB.Next(0, 2);
-            }
-  
+            //}
+
             //_stopwatch.Start();
             //response2 = test2.Solution(C, D);
             //_stopwatch.Stop();
@@ -46,7 +63,9 @@ namespace IanAQ2IntAns
             //{
             //    Console.WriteLine("The number of fish that survived this trip where {0}. This to {1} to work out", response2, _stopwatch.Elapsed.ToString());
             //}
+            response = test3.Solution(TreeA);
 
+            Console.WriteLine("Number of nodes is {0}", response);
 
             Console.WriteLine("Press Enter to close");
             Console.ReadLine();
@@ -90,7 +109,7 @@ namespace IanAQ2IntAns
     {
         public int Solution(int[] A, int[] B)
         {
-            Stack<int> NoOfFishAlive = new Stack<int>();            
+            Stack<int> NoOfFishAlive = new Stack<int>();
             for (int i = 0; i < A.Length; i++)
             {
                 if (NoOfFishAlive.Count == 0)
@@ -99,8 +118,8 @@ namespace IanAQ2IntAns
                 }
                 else
                 {
-                    while (NoOfFishAlive.Count != 0 && 
-                        B[i] - B[NoOfFishAlive.Peek()]  == -1 && 
+                    while (NoOfFishAlive.Count != 0 &&
+                        B[i] - B[NoOfFishAlive.Peek()] == -1 &&
                         A[NoOfFishAlive.Peek()] < A[i])
                     {
                         NoOfFishAlive.Pop();
@@ -113,49 +132,45 @@ namespace IanAQ2IntAns
                             NoOfFishAlive.Push(i);
                         }
                     }
-                    else { NoOfFishAlive.Push(i); }                    
+                    else { NoOfFishAlive.Push(i); }
                 }
             }
-            return NoOfFishAlive.Count;            
-            
-            /*          int NoOfFishAlive = 0, FishEaten = A.Length;
-                      int N = A.Length;
-                      while (FishEaten > 0)
-                      {
-                          for (int k = 0; k < N / 2; k++)
-                          {
-                              FishEaten = 0;
-                              for (int i = 0, j = 1; i < N - 1; i++, j++)
-                              {
-                                  if (A[i] != 0)
-                                  {
-                                      if (B[i] == 1 && B[j] == 0)
-                                      {
-                                          if (A[i] > A[j])
-                                          {
-                                              A[j] = 0;
-                                              FishEaten = +1;
-                                          }
-                                          else if (A[i] < A[j])
-                                          {
-                                              A[i] = 0;
-                                              FishEaten = +1;
-                                          }
-                                      }
-                                  }
-                              }
-                          }
-                      }
-                      for (int i = 0; i < N; i++)
-                      {
-                          if (A[i] > 0)
-                          {
-                              NoOfFishAlive += 1;
-                          }
-                      }*/
+            return NoOfFishAlive.Count;
         }
     }
-    class Tree
+    public class Tree_Solution
+    {
+        public int Solution(Tree A)
+        {
+            Hashtable t = new Hashtable();
+            if (A != null)
+            {
+                getMaxNumberOfNodes(A, t);
+                return t.Count;
+            }
+            else return 0;
+        }
+        public void getMaxNumberOfNodes(Tree node, Hashtable t)
+        {
+            if (node != null)
+            {
+                if (!t.ContainsKey(node.x))
+                {
+                    t.Add(node.x, node);
+                }
+                getMaxNumberOfNodes(node.r, t);
+                getMaxNumberOfNodes(node.l, t);
+            }
+        }
+        public Tree AddNewNode(int value)
+        {
+            Tree A = new Tree();
+            A.x = value;
+            return A;
+        }
+    }
+
+    public class Tree
     {
         public int x;
         public Tree l;
